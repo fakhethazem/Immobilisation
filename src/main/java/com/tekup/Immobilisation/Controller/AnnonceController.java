969fee78df334d0a30209f38b594e51f25aa2a38
unknown <hazemfakhet08@gmail.com>
2021-11-18 18:32:37 +0100

    web scraping

--- src/main/java/com/tekup/Immobilisation/Controller/AnnonceController.java
+++ src/main/java/com/tekup/Immobilisation/Controller/AnnonceController.java
@@ -1,8 +1,10 @@
 package com.tekup.Immobilisation.Controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +17,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.tekup.Immobilisation.Services.ClientServiceImp;
 import com.tekup.Immobilisation.Services.IAnnonceService;
 import com.tekup.Immobilisation.Services.IClientService;
+import com.tekup.Immobilisation.Services.JsoupService;
 import com.tekup.Immobilisation.entities.Annonce;
+import com.tekup.Immobilisation.entities.AnnonceDto;
 import com.tekup.Immobilisation.entities.Client;
 
 import lombok.AllArgsConstructor;
@@ -29,12 +35,27 @@ public class AnnonceController {
 
 @Autowired
 private IAnnonceService annonceServiceImp;
+@Autowired
+private JsoupService jsoupService;
 
 
 @GetMapping("/annonces")
 public List<Annonce> getAllfromDB(){
 	return annonceServiceImp.getAllAnnonces();
 }
+
+@GetMapping("/TunisiaAnnonce")
+public List<AnnonceDto> extractData() {
+	 jsoupService.getDataFromtunisiaAnnonce();
+	 return jsoupService.list_ann;
+}
+
+/*@GetMapping("/TunisiaAnnonce/{region}")
+public List<AnnonceDto> extractDatabyregion(@PathVariable String region){
+	 jsoupService.getDataFromtunisiaAnnonce();
+	return jsoupService.getDataByRegion(region);
+}*/
+
 
 
 @GetMapping("/annonces/{id}")
--- src/main/java/com/tekup/Immobilisation/ImmobilisationApplication.java
+++ src/main/java/com/tekup/Immobilisation/ImmobilisationApplication.java
@@ -1,13 +1,15 @@
 package com.tekup.Immobilisation;
-
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class ImmobilisationApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ImmobilisationApplication.class, args);
+		
 	}
-
 }
