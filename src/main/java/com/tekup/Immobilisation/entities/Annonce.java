925c1affe6b54422188bcc7e29b04f60e3e2aae7
unknown <hazemfakhet08@gmail.com>
2021-12-27 00:19:33 +0100

    image upload

--- src/main/java/com/tekup/Immobilisation/entities/Annonce.java
+++ src/main/java/com/tekup/Immobilisation/entities/Annonce.java
@@ -6,24 +6,19 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import org.hibernate.boot.model.relational.Database;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import lombok.Data;
+
 
 @Entity
 @Table(name="Annonce")
